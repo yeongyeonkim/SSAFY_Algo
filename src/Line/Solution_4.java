15a671cbbef39d85a5d3d7f0cb462297fdb2bf65
KimYeongYeon <45118806+yeongyeonkim@users.noreply.github.com>
2019-09-23 10:09:31 +0900

    Update Solution_4.java

--- src/Line/Solution_4.java
+++ src/Line/Solution_4.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//틀린코드입니다.
 public class Solution_4 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
